1a7601e53192dd2895e641134c70499113c05f55
Azar <azar.barfipour@gmail.com>
2021-09-18 15:59:50 -0700

    header

--- Fisrtproject/my-first-app/src/App.js
+++ Fisrtproject/my-first-app/src/App.js
@@ -1,11 +1,9 @@
-import Todo from './components/Todo';
+import Header from "./components/Header";
+
 function App() {
   return(
     <div>
-      <h1>My Todo</h1>
-      <Todo text='title'/>
-      <Todo text='title2'/>
-      <Todo text='title3'/>
+      <Header />
     </div>
   );
 }
---
+++ Fisrtproject/my-first-app/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Hello World</h1>
+        </header>
+    )
+}
+
+export default Header
