f7ea1ffe3603b5e60265b806ca3186196cdd5b14
Azar <azar.barfipour@gmail.com>
2021-09-18 15:23:42 -0700

    react test commit

---
+++ Fisrtproject/my-first-app/src/App.js
@@ -0,0 +1,13 @@
+import Todo from './components/Todo';
+function App() {
+  return(
+    <div>
+      <h1>My Todo</h1>
+      <Todo text='title'/>
+      <Todo text='title2'/>
+      <Todo text='title3'/>
+    </div>
+  );
+}
+
+export default App;
