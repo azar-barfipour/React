8c9f17a88e47afb8ecf579d42b24f7f4363dc07c
Azar <azar.barfipour@gmail.com>
2021-09-25 13:31:44 -0700

    add find method

--- Udemy/map/script.js
+++ Udemy/map/script.js
@@ -1,4 +1,4 @@
-//map: it creates another array so first array has the same values
+//map: creates another array so first array has the same values
 
 const number = [1, 2, 3];
 const doubleNumber = number.map((num) => {
@@ -6,3 +6,9 @@ const doubleNumber = number.map((num) => {
 });
 console.log(number);
 console.log(doubleNumber);
+
+//find: returns the first element which is match with the codition
+const array1 = [5, 12, 8, 130, 44];
+const find = array1.find((el) => el >= 10);
+
+console.log(find);
