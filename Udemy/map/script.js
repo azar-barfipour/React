5e406c9e49c0049b00f1069945c8a0ee59201776
Azar <azar.barfipour@gmail.com>
2021-09-25 13:46:29 -0700

    add findindex method

--- Udemy/map/script.js
+++ Udemy/map/script.js
@@ -1,4 +1,4 @@
-//map: creates another array so first array has the same values
+//map(): creates another array so first array has the same values
 
 const number = [1, 2, 3];
 const doubleNumber = number.map((num) => {
@@ -7,8 +7,13 @@ const doubleNumber = number.map((num) => {
 console.log(number);
 console.log(doubleNumber);
 
-//find: returns the first element which is match with the codition
+//find(): returns the first element which is match with the codition
 const array1 = [5, 12, 8, 130, 44];
 const find = array1.find((el) => el >= 10);
 
 console.log(find);
+
+//findindex(): returns the first index of element which is match with the condition
+const isLargerThanTwelve = array1.findIndex((el) => el > 12);
+
+console.log(isLargerThanTwelve);
