2f6b9902674444088b2f60b26bac707a83347ff8
Azar <azar.barfipour@gmail.com>
2021-09-25 15:48:26 -0700

    add concat

--- Udemy/map/script.js
+++ Udemy/map/script.js
@@ -10,10 +10,30 @@ console.log(doubleNumber);
 //find(): returns the first element which is match with the codition
 const array1 = [5, 12, 8, 130, 44];
 const find = array1.find((el) => el >= 10);
-
+console.log(array1);
 console.log(find);
 
 //findindex(): returns the first index of element which is match with the condition
 const isLargerThanTwelve = array1.findIndex((el) => el > 12);
-
+console.log(array1);
 console.log(isLargerThanTwelve);
+
+//filter(): creates another array with all element that match with the codition
+const filter = array1.filter((el) => el >= 12);
+console.log(array1);
+console.log(filter);
+
+//reduce(): returns sum of the all elemts in the array
+const reduce = array1.reduce((preValue, nextValue) => {
+  return preValue + nextValue;
+});
+console.log(array1);
+console.log(reduce);
+
+//concat(): creates another array which is arr1 + arr2
+
+const array2 = [8, 10, 50, 60];
+const array3 = array1.concat(array2);
+console.log(array1);
+console.log(array2);
+console.log(array3);
