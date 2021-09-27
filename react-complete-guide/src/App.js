9e4e1feb3e881cea2aca45e78f45ac8b029d4cca
Azar <azar.barfipour@gmail.com>
2021-09-26 18:07:35 -0700

    add component for date

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,26 +1,50 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  const Hello = [
+  const expenses = [
     {
-      title: "Hello worls",
-      age: 30,
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      title: "Hello Azar",
-      age: 31,
-    },
-    {
-      title: "Hello Azar khoshgele",
-      age: 32,
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={Hello[0].title} age={Hello[0].age} />
-      <ExpenseItem title={Hello[1].title} age={Hello[1].age} />
-      <ExpenseItem title={Hello[2].title} age={Hello[2].age} />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,8 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
-    <h1>
-      {props.title} {props.age}
-    </h1>
+    <div className="expense-item__description">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
   );
 }
 
