15ea6b2ce748d3f8a94f991a16c10de4d2a9fe76
Azar <azar.barfipour@gmail.com>
2021-09-26 18:59:30 -0700

    add component for expences

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,6 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+import Header from "./Header";
 
 function App() {
   const expenses = [
@@ -24,27 +26,8 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Header />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ react-complete-guide/src/Header.js
@@ -0,0 +1,4 @@
+function Header() {
+  return <h2>Let's get started!</h2>;
+}
+export default Header;
