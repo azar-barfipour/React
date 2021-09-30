811823e3c70e7f322730eb24e48aa4efde8fe251
Azar <azar.barfipour@gmail.com>
2021-09-29 21:48:07 -0700

    get object of new form

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -13,12 +13,30 @@ const ExpenseForm = () => {
   const dateChangeHandler = (event) => {
     setenteredDate(event.target.value);
   };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: setenteredTitle,
+      amount: setenteredAmount,
+      date: new Date(setenteredDate),
+    };
+    // console.log(expenseData);
+    setenteredTitle("");
+    setenteredAmount("");
+    setenteredDate("");
+  };
+
   return (
-    <form>
+    <form onClick={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -26,6 +44,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -35,6 +54,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
