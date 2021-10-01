d6b2984bdb2abe267eb1a3971dc164d3d2805048
Azar <azar.barfipour@gmail.com>
2021-09-30 17:41:29 -0700

    update expenses dinamic

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,35 +1,27 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [changeYear, setChangeYear] = useState("2020");
   const saveExpensesFilterHandler = (expensesFilterData) => {
-    console.log(expensesFilterData);
+    console.log(setChangeYear(expensesFilterData));
   };
   return (
     <Card className="expenses">
-      <ExpensesFilter onExpensesFilter={saveExpensesFilterHandler} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+      <ExpensesFilter
+        selected={changeYear}
+        onExpensesFilter={saveExpensesFilterHandler}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
--- react-complete-guide/src/components/Expenses/ExpensesFilter.js
+++ react-complete-guide/src/components/Expenses/ExpensesFilter.js
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-  const [enteresYear, setenteresYear] = useState("");
-
   const filterHandler = (event) => {
-    props.onExpensesFilter(event.target.value);
+    console.log(props.onExpensesFilter(event.target.value));
   };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={filterHandler}>
-          <option value={enteresYear}>2022</option>
-          <option value={enteresYear}>2021</option>
-          <option value={enteresYear}>2020</option>
-          <option value={enteresYear}>2019</option>
+        <select value={props.selected} onChange={filterHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
