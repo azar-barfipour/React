7aed5b4c197fa81516314d7e66b472f6849a986d
Azar <azar.barfipour@gmail.com>
2021-10-02 16:35:32 -0700

    add filter for new expenses

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,17 +1,33 @@
+import react, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [clicked, setClicked] = useState(false);
   const saveDataHandler = (enterExpenseData) => {
     const expenseData = {
       ...enterExpenseData,
       id: 1,
     };
     props.onAddNewExpens(expenseData);
+    setClicked(false);
+  };
+  const newButtonHandler = () => {
+    setClicked(true);
+  };
+  const cancleAddingExpense = () => {
+    setClicked(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveDataHandler} />
+      {clicked ? (
+        <ExpenseForm
+          onSaveExpenseData={saveDataHandler}
+          cancleHandler={cancleAddingExpense}
+        />
+      ) : (
+        <button onClick={newButtonHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
