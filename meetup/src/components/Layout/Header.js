492e280236db028d6926717e0f0d4e05a4978782
Azar <azar.barfipour@gmail.com>
2021-10-20 16:51:29 -0700

    add header

---
+++ meetup/src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <h1>meetup</h1>
+    </div>
+  );
+};
+
+export default Header;
