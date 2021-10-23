fb306566e4a1a1d4151e3419d71834bb52b3119e
Azar <azar.barfipour@gmail.com>
2021-10-22 21:01:24 -0700

    add date and time

--- meetup/src/components/Calendar/Calendar.js
+++ meetup/src/components/Calendar/Calendar.js
@@ -7,16 +7,19 @@ const Calendar = () => {
       id: "c1",
       title: "Dance Club",
       description: "Having fun and dancing in a great area",
+      date: new Date(1990, 12, 2, 12, 30),
     },
     {
       id: "c2",
       title: "English Conversation",
       description: "an online event for speaking in English",
+      date: new Date(2020, 12, 5, 13, 20),
     },
     {
       id: "c3",
       title: "Persian Dance",
       description: "Art gallery with great DJs",
+      date: new Date(2021, 8, 7, 16, 40),
     },
   ];
   return (
@@ -31,6 +34,7 @@ const Calendar = () => {
               <CalendarItems
                 title={calendarIteme.title}
                 dec={calendarIteme.description}
+                date={calendarIteme.date}
               />
             );
           })}
--- meetup/src/components/Calendar/CalendarItems.js
+++ meetup/src/components/Calendar/CalendarItems.js
@@ -2,11 +2,18 @@ import classes from "./CalendarItems.module.css";
 import Card from "../UI/Card";
 
 const CalendarItems = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "short" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  const hour = props.date.getHours();
+  const minute = props.date.getMinutes();
+  const date = year + " " + month + " " + day + " " + hour + " : " + minute;
   return (
     <Card className={classes.calendarItems}>
       <li>
         <h4>{props.title}</h4>
         <p>{props.dec}</p>
+        <p>{date}</p>
       </li>
     </Card>
   );
