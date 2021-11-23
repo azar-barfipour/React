7afc7ec85843a127d832457d1dad3c4e4d61a94b
Azar <azar.barfipour@gmail.com>
2021-11-23 13:59:08 -0800

    return images temporary

--- meetup/src/components/Groups/YourGroups/YourGroupsDetails.js
+++ meetup/src/components/Groups/YourGroups/YourGroupsDetails.js
@@ -4,6 +4,12 @@ const YourGroupsDetails = (props) => {
   return (
     <Card className={classes.details}>
       <li>
+        <img
+          // src={props.image}
+          src="https://images.unsplash.com/photo-1637270057940-921ced86faee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+          alt="theme event"
+          className={classes["gropuItem__image"]}
+        />
         <h5>{props.title}</h5>
       </li>
     </Card>
