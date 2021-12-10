828224f934c48ed84914853760a6d3162480204c
Azar Barfipour <azar.barfipour@gmail.com>
2021-12-10 10:10:54 -0800

    compelete mobile-first home

--- meetup/src/components/Layout/Footer.js
+++ meetup/src/components/Layout/Footer.js
@@ -1,8 +1,37 @@
 import classes from "./Footer.module.css";
+import { NavLink } from "react-router-dom";
+import {useContext} from 'react'
+import AuthContext from "../../store/auth-context";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
+  const autCtx = useContext(AuthContext);
+  const isLoggedIn = autCtx.isLoggedIn;
   return (
     <div className={classes.footer}>
-      <h2>Footer Menu</h2>
+      <nav className={classes['footer__nav']}>
+        <ul className={classes['footer__list']}>
+          <li className={classes['footer__list--item']}> 
+            <NavLink  activeClassName={classes.active} to="/Home">
+              <div className={classes['footer__list--link']}>
+              <FontAwesomeIcon icon={faHome}/>
+              <p className={classes['footer__list--title']}>Home</p>
+              </div>
+            </NavLink>
+          </li>
+          {isLoggedIn && (
+            <li className={classes['footer__list--item']}>
+              <NavLink activeClassName={classes.active} to="/Explore">
+              <div className={classes['footer__list--link']}>
+              <FontAwesomeIcon icon={faSearch}/>
+              <p className={classes['footer__list--title']}>Explore</p>
+              </div>
+              </NavLink>
+            </li>
+          )}
+        </ul>
+      </nav>
     </div>
   );
 };
