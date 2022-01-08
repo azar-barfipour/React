2ff79f4bca3bcb7d6fccc66445e2c5703b4a8e4f
Azar Barfipour <azar.barfipour@gmail.com>
2022-01-07 22:23:24 -0800

    added filtering ui working

--- meetup/src/components/Search/SearchDate.js
+++ meetup/src/components/Search/SearchDate.js
@@ -2,36 +2,68 @@ import { useState } from 'react';
 import classes from './SearchDate.module.css';
 const SearchDate = () => {
 const [text,setText] = useState();
-const [isClicked, setIsClicked] = useState(false);
-const spanLinkHandler = (event) => {
-    setIsClicked(true)
+const [isClicked1, setIsClicked1] = useState(false);
+const [isClicked2, setIsClicked2] = useState(false);
+const [isClicked3, setIsClicked3] = useState(false);
+const [isClicked4, setIsClicked4] = useState(false);
+const spanLinkHandler1 = (event) => {
+    setIsClicked1(true)
     event.preventDefault();
     setText(event.target.innerText);
 }
-const closeHandler = (event) => {
-    setIsClicked(false);
+const spanLinkHandler2 = (event) => {
+    setIsClicked2(true)
+    event.preventDefault();
+    setText(event.target.innerText);
+}
+const spanLinkHandler3 = (event) => {
+    setIsClicked3(true)
+    event.preventDefault();
+    setText(event.target.innerText);
+}
+const spanLinkHandler4 = (event) => {
+    setIsClicked4(true)
+    event.preventDefault();
+    setText(event.target.innerText);
+}
+const closeHandler1 = (event) => {
+    setIsClicked1(false);
+    event.preventDefault();
+    setText(null)
+}
+const closeHandler2 = (event) => {
+    setIsClicked2(false);
+    event.preventDefault();
+    setText(null)
+}
+const closeHandler3 = (event) => {
+    setIsClicked3(false);
+    event.preventDefault();
+    setText(null)
+}
+const closeHandler4 = (event) => {
+    setIsClicked4(false);
     event.preventDefault();
     setText(null)
 }
 console.log(text);
-console.log(isClicked);
     return <section className={classes['span-wrapper']}>
         <ul className={classes['span__list']}>
-        <li className={`${classes['span__item']} ${isClicked && classes['span__item--color']}`}>
-            <a className={classes['span__link']} onClick={spanLinkHandler} href='#'>Today</a>
-            <span onClick={closeHandler}>&#10005;</span>
+        <li className={`${classes['span__item']} ${isClicked1 && classes['span__item--color1']}`}>
+            <a className={classes['span__link']} onClick={spanLinkHandler1} href='#'>Today</a>
+            <span onClick={closeHandler1}>&#10005;</span>
         </li>
-        <li className={`${classes['span__item']} ${isClicked && classes['span__item--color']}`}>
-            <a className={classes['span__link']}  onClick={spanLinkHandler} href='#'>Tommorow</a>
-            <span onClick={closeHandler}>&#10005;</span>
+        <li className={`${classes['span__item']} ${isClicked2 && classes['span__item--color2']}`}>
+            <a className={classes['span__link']}  onClick={spanLinkHandler2} href='#'>Tommorow</a>
+            <span onClick={closeHandler2}>&#10005;</span>
             </li>
-        <li className={`${classes['span__item']} ${isClicked && classes['span__item--color']}`}>
-            <a className={classes['span__link']} onClick={spanLinkHandler} href='#'>This Week</a>
-            <span onClick={closeHandler}>&#10005;</span>
+        <li className={`${classes['span__item']} ${isClicked3 && classes['span__item--color3']}`}>
+            <a className={classes['span__link']} onClick={spanLinkHandler3} href='#'>This Week</a>
+            <span onClick={closeHandler3}>&#10005;</span>
             </li>
-        <li className={`${classes['span__item']} ${isClicked && classes['span__item--color']}`}>
-            <a className={classes['span__link']} onClick={spanLinkHandler} href='#'>This Month</a>
-            <span onClick={closeHandler}>&#10005;</span>
+        <li className={`${classes['span__item']} ${isClicked4 && classes['span__item--color4']}`}>
+            <a className={classes['span__link']} onClick={spanLinkHandler4} href='#'>This Month</a>
+            <span onClick={closeHandler4}>&#10005;</span>
             </li>
         </ul>
     </section>
