951f4b556a1574cdc20447fc997633a31e150546
Azar Barfipour <azar.barfipour@gmail.com>
2022-01-09 18:51:49 -0800

    added fliter for today

--- meetup/src/components/Search/SearchDate.js
+++ meetup/src/components/Search/SearchDate.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import classes from './SearchDate.module.css';
-const SearchDate = () => {
-const [text,setText] = useState();
+const SearchDate = (props) => {
+const [text1,setText1] = useState();
+const [text2,setText2] = useState();
+const [text3,setText3] = useState();
+const [text4,setText4] = useState();
 const [isClicked1, setIsClicked1] = useState(false);
 const [isClicked2, setIsClicked2] = useState(false);
 const [isClicked3, setIsClicked3] = useState(false);
@@ -9,60 +12,68 @@ const [isClicked4, setIsClicked4] = useState(false);
 const spanLinkHandler1 = (event) => {
     setIsClicked1(true)
     event.preventDefault();
-    setText(event.target.innerText);
+    props.onGetText(event.target.innerText);
+    setText1(event.target.innerText);
 }
 const spanLinkHandler2 = (event) => {
     setIsClicked2(true)
     event.preventDefault();
-    setText(event.target.innerText);
+    props.onGetText(event.target.innerText);
+    setText2(event.target.innerText);
 }
 const spanLinkHandler3 = (event) => {
     setIsClicked3(true)
     event.preventDefault();
-    setText(event.target.innerText);
+    props.onGetText(event.target.innerText);
+    setText3(event.target.innerText);
 }
 const spanLinkHandler4 = (event) => {
     setIsClicked4(true)
     event.preventDefault();
-    setText(event.target.innerText);
+    props.onGetText(event.target.innerText);
+    setText4(event.target.innerText);
 }
 const closeHandler1 = (event) => {
     setIsClicked1(false);
     event.preventDefault();
-    setText(null)
+    // props.onGetText(event.target.innerText);
+    setText1(null)
 }
 const closeHandler2 = (event) => {
     setIsClicked2(false);
     event.preventDefault();
-    setText(null)
+    // props.onGetText(event.target.innerText);
+    setText2(null)
 }
 const closeHandler3 = (event) => {
     setIsClicked3(false);
     event.preventDefault();
-    setText(null)
+    // props.onGetText(event.target.innerText);
+    setText3(null)
 }
 const closeHandler4 = (event) => {
     setIsClicked4(false);
     event.preventDefault();
-    setText(null)
+    // props.onGetText(event.target.innerText);
+    setText4(null)
 }
-console.log(text);
+// props.onGetText([text1,text2,text3,text4]);
     return <section className={classes['span-wrapper']}>
         <ul className={classes['span__list']}>
         <li className={`${classes['span__item']} ${isClicked1 && classes['span__item--color1']}`}>
-            <a className={classes['span__link']} onClick={spanLinkHandler1} href='#'>Today</a>
+            <button className={classes['span__link']} onClick={spanLinkHandler1} type='button'>Today</button>
             <span onClick={closeHandler1}>&#10005;</span>
         </li>
         <li className={`${classes['span__item']} ${isClicked2 && classes['span__item--color2']}`}>
-            <a className={classes['span__link']}  onClick={spanLinkHandler2} href='#'>Tommorow</a>
+            <button className={classes['span__link']}  onClick={spanLinkHandler2} type='button'>Tommorow</button>
             <span onClick={closeHandler2}>&#10005;</span>
             </li>
         <li className={`${classes['span__item']} ${isClicked3 && classes['span__item--color3']}`}>
-            <a className={classes['span__link']} onClick={spanLinkHandler3} href='#'>This Week</a>
+            <button className={classes['span__link']} onClick={spanLinkHandler3} type='button'>This Week</button>
             <span onClick={closeHandler3}>&#10005;</span>
             </li>
         <li className={`${classes['span__item']} ${isClicked4 && classes['span__item--color4']}`}>
-            <a className={classes['span__link']} onClick={spanLinkHandler4} href='#'>This Month</a>
+            <button className={classes['span__link']} onClick={spanLinkHandler4} type='button'>This Month</button>
             <span onClick={closeHandler4}>&#10005;</span>
             </li>
         </ul>
--- meetup/src/components/Search/SearchForm.js
+++ meetup/src/components/Search/SearchForm.js
@@ -10,9 +10,42 @@ const SearchForm = () => {
   const [enteredSearch, setEnteredSearch] = useState("");
   const [events, setEvents] = useState([]);
   const [eventsResult, setEventsResult] = useState([]);
+  const [eventsResult2, setEventsResult2] = useState([]);
+  const [events2, setEvents2] = useState(false);
+  // const [text,setText] = useState([])
   const inputChangeHandler = (event) => {
     setEnteredSearch(event.target.value);
   };
+  const getTextHandler = (text) => {
+    // setText(text)
+    // for(const i of events){
+    //  const date = i.date.slice(0,10);
+    //  console.log(date);
+    // }
+    // setText((prevTexts) => {
+    //   return [text, ...prevTexts];
+    // });
+    setEvents2(true);
+    // const result = events.filter((e) =>
+    //   (e.date.includes(text))
+    //     )
+    const today= new Date().toISOString();
+    const tommorow= new Date().toISOString() + 1;
+    console.log(today);
+    console.log(tommorow);
+    // console.log(tommorow.toISOString().slice(0,10));
+    console.log(events);
+    if(text === 'Today') {
+    const result = events.filter(e => e.date.slice(0,10) === new Date().toISOString().slice(0,10))
+    console.log(result);
+      setEventsResult2(result);
+  }else if(text === 'Tommorow'){
+    const result = events.filter(e => e.date.slice(0,10) === tommorow.slice(0,10))
+    console.log(result);
+      setEventsResult2(result);
+  }
+} 
+  // console.log(eventsResult2);
   useEffect(() => {
     const fetchHandler = async () => {
       const response = await fetch(
@@ -21,17 +54,16 @@ const SearchForm = () => {
       const data = await response.json();
       let loadedData = [];
       for (const key in data) {
-        loadedData.push({ id: key, title: data[key].title });
+        loadedData.push({ id: key, title: data[key].title ,date:data[key].date });
       }
       setEvents(loadedData);
       const result = events.filter((e) =>
-        e.title.toLowerCase().includes(enteredSearch)
-        )
+      e.title.toLowerCase().includes(enteredSearch))
+        // console.log(result);
       setEventsResult(result);
     };
     fetchHandler();
   }, [enteredSearch]);
-
   return (
     <div className={classes["form-container"]}>
       <form className={classes.form}>
@@ -44,9 +76,10 @@ const SearchForm = () => {
         />
         <FontAwesomeIcon icon={faSearch} className={classes["form__icon"]}/>
       </form>
-      <SearchDate/>
-      {!enteredSearch && <SearchItems events={events} />}
-      {enteredSearch && <SearchItems events={eventsResult} />}
+      <SearchDate onGetText={getTextHandler}/>
+      {!enteredSearch && !events2 && <SearchItems events={events} />}
+      {enteredSearch  && <SearchItems events={eventsResult} />}
+      {events2 && <SearchItems events={eventsResult2} />}
     </div>
   );
 };
